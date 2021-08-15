874d5ac7fb9561c2fd16774db8b089ff29fe2829
Amarjeet Verma <amarjeet.verma1987@gmail.com>
2021-08-15 15:31:28 +0200

    first commit

---
+++ src/test/java/com/framework/MyFirstTest.java
@@ -0,0 +1,23 @@
+package com.framework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    void MyFirstTest() {
+        System.out.println("Runs!");
+        int result = sum(2, 2);
+        Assertions.assertEquals(4, result);
+    }
+
+
+    static int sum(int a, int b)
+    {
+        return a + b;
+    }
+                        }
+
+
+
